020b7c24bda60ee3fdc69c1272d31af85d1b8d44
Shadi Shaheen <sshahine@ccc.com.lb>
2014-11-07 12:32:49 +0200

    adding the old anchor pane in 1.8u5 to rippler instead of using the u25 anchorpane

--- src/customui/components/Rippler.java
+++ src/customui/components/Rippler.java
@@ -1,5 +1,7 @@
 package customui.components;
 
+import java.util.List;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
@@ -7,6 +9,8 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.Control;
@@ -21,11 +25,10 @@ import javafx.util.Duration;
 
 public class Rippler extends StackPane {
 
-
 	public static enum RipplerPos{FRONT, BACK};
 	public static enum RipplerMask{CIRCLE, RECT};
 	
-	private AnchorPane ripplerPane;
+	private C3DAnchorPane ripplerPane;
 	private RipplerMask maskType = RipplerMask.RECT ;
 	private RipplerPos pos = RipplerPos.FRONT ;
 	private boolean enabled = true;
@@ -51,12 +54,11 @@ public class Rippler extends StackPane {
 		// create rippler panels
 		
 		final RippleGenerator rippler = new RippleGenerator();
-		ripplerPane = new AnchorPane();
+		ripplerPane = new C3DAnchorPane();
 		ripplerPane.getChildren().add(rippler);
 				
 		if(this.pos == RipplerPos.BACK)  ripplerPane.getChildren().add(this.control);
 		else this.getChildren().add(this.control);
-		
 		this.getChildren().add(ripplerPane);
 
 		if(this.control instanceof Control){
@@ -134,9 +136,9 @@ public class Rippler extends StackPane {
 		public void createRipple() {
 			if(enabled){
 				final Ripple ripple = new Ripple(generatorCenterX, generatorCenterY);				
-				Shape mask = new Rectangle(ripplerPane.getWidth()-0.1,ripplerPane.getHeight()-0.1); // -0.1 to prevent resizing the anchor pane
+				Shape mask = new Rectangle(ripplerPane.getWidth() - 0.1,ripplerPane.getHeight() - 0.1); // -0.1 to prevent resizing the anchor pane
 				if(maskType.equals(Rippler.RipplerMask.CIRCLE)){
-					mask = new Circle(ripplerPane.getWidth()/2 , ripplerPane.getHeight()/2, (ripplerPane.getWidth()/2) - 0.1 , Color.BLUE);	
+					mask = new Circle(ripplerPane.getWidth()/2 , ripplerPane.getHeight()/2, (ripplerPane.getWidth()/2) - 0.1, Color.BLUE);	
 				}
 				ripple.setClip(mask);
 				getChildren().add(ripple);
@@ -176,8 +178,157 @@ public class Rippler extends StackPane {
 				setFill(circleColor);
 			}
 		}
-
-
 	}
 
+	private class C3DAnchorPane extends AnchorPane{
+		
+	    @Override protected double computeMinWidth(double height) {
+	        return computeWidth(true, height);
+	    }
+
+	    @Override protected double computeMinHeight(double width) {
+	        return computeHeight(true, width);
+	    }
+
+	    @Override protected double computePrefWidth(double height) {
+	        return computeWidth(false, height);
+	    }
+
+	    @Override protected double computePrefHeight(double width) {
+	        return computeHeight(false, width);
+	    }
+		
+		 private double computeWidth(final boolean minimum, final double height) {
+		        double max = 0;
+		        double contentHeight = height != -1 ? height - getInsets().getTop() - getInsets().getBottom() : -1;
+		        final List<Node> children = getManagedChildren();
+		        for (Node child : children) {
+		            Double leftAnchor = getLeftAnchor(child);
+		            Double rightAnchor = getRightAnchor(child);
+
+		            double left = leftAnchor != null? leftAnchor :
+		                (rightAnchor != null? 0 : child.getLayoutBounds().getMinX() + child.getLayoutX());
+		            double right = rightAnchor != null? rightAnchor : 0;
+		            double childHeight = -1;
+		            if (child.getContentBias() == Orientation.VERTICAL && contentHeight != -1) {
+		                // The width depends on the node's height!
+		                childHeight = computeChildHeight(child, getTopAnchor(child), getBottomAnchor(child), contentHeight, -1);
+		            }
+		            max = Math.max(max, left + (minimum && leftAnchor != null && rightAnchor != null?
+		                    child.minWidth(childHeight) : child.prefWidth(childHeight)) + right);
+		        }
+
+		        final Insets insets = getInsets();
+		        return insets.getLeft() + max + insets.getRight();
+		    }
+
+		    private double computeHeight(final boolean minimum, final double width) {
+		        double max = 0;
+		        double contentWidth = width != -1 ? width - getInsets().getLeft()- getInsets().getRight() : -1;
+		        final List<Node> children = getManagedChildren();
+		        for (Node child : children) {
+		            Double topAnchor = getTopAnchor(child);
+		            Double bottomAnchor = getBottomAnchor(child);
+
+		            double top = topAnchor != null? topAnchor :
+		                (bottomAnchor != null? 0 : child.getLayoutBounds().getMinY() + child.getLayoutY());
+		            double bottom = bottomAnchor != null? bottomAnchor : 0;
+		            double childWidth = -1;
+		            if (child.getContentBias() == Orientation.HORIZONTAL && contentWidth != -1) {
+		                childWidth = computeChildWidth(child, getLeftAnchor(child), getRightAnchor(child), contentWidth, -1);
+		            }
+		            max = Math.max(max, top + (minimum && topAnchor != null && bottomAnchor != null?
+		                    child.minHeight(childWidth) : child.prefHeight(childWidth)) + bottom);
+		        }
+
+		        final Insets insets = getInsets();
+		        return insets.getTop() + max + insets.getBottom();
+		    }
+		    private double computeChildWidth(Node child, Double leftAnchor, Double rightAnchor, double areaWidth, double height) {
+		        if (leftAnchor != null && rightAnchor != null && child.isResizable()) {
+		            final Insets insets = getInsets();
+		            return areaWidth - insets.getLeft() - insets.getRight() - leftAnchor - rightAnchor;
+		        }
+		        return computeChildPrefAreaWidth(child, -1, Insets.EMPTY, height, true);
+		    }
+
+		    private double computeChildHeight(Node child, Double topAnchor, Double bottomAnchor, double areaHeight, double width) {
+		        if (topAnchor != null && bottomAnchor != null && child.isResizable()) {
+		            final Insets insets = getInsets();
+		            return areaHeight - insets.getTop() - insets.getBottom() - topAnchor - bottomAnchor;
+		        }
+		        return computeChildPrefAreaHeight(child, -1, Insets.EMPTY, width);
+		    }
+		    
+
+		    double computeChildPrefAreaWidth(Node child, double baselineComplement, Insets margin, double height, boolean fillHeight) {
+		        final boolean snap = isSnapToPixel();
+		        double left = margin != null? snapSpace(margin.getLeft(), snap) : 0;
+		        double right = margin != null? snapSpace(margin.getRight(), snap) : 0;
+		        double alt = -1;
+		        if (height != -1 && child.isResizable() && child.getContentBias() == Orientation.VERTICAL) { // width depends on height
+		            double top = margin != null? snapSpace(margin.getTop(), snap) : 0;
+		            double bottom = margin != null? snapSpace(margin.getBottom(), snap) : 0;
+		            double bo = child.getBaselineOffset();
+		            final double contentHeight = bo == BASELINE_OFFSET_SAME_AS_HEIGHT && baselineComplement != -1 ?
+		                    height - top - bottom - baselineComplement :
+		                     height - top - bottom;
+		            if (fillHeight) {
+		                alt = snapSize(boundedSize(
+		                        child.minHeight(-1), contentHeight,
+		                        child.maxHeight(-1)));
+		            } else {
+		                alt = snapSize(boundedSize(
+		                        child.minHeight(-1),
+		                        child.prefHeight(-1),
+		                        Math.min(child.maxHeight(-1), contentHeight)));
+		            }
+		        }
+		        return left + snapSize(boundedSize(child.minWidth(alt), child.prefWidth(alt), child.maxWidth(alt))) + right;
+		    }
+		    
+		    double computeChildPrefAreaHeight(Node child, double prefBaselineComplement, Insets margin, double width) {
+		        final boolean snap = isSnapToPixel();
+		        double top = margin != null? snapSpace(margin.getTop(), snap) : 0;
+		        double bottom = margin != null? snapSpace(margin.getBottom(), snap) : 0;
+
+		        double alt = -1;
+		        if (child.isResizable() && child.getContentBias() == Orientation.HORIZONTAL) { // height depends on width
+		            double left = margin != null ? snapSpace(margin.getLeft(), snap) : 0;
+		            double right = margin != null ? snapSpace(margin.getRight(), snap) : 0;
+		            alt = snapSize(boundedSize(
+		                    child.minWidth(-1), width != -1 ? width - left - right
+		                    : child.prefWidth(-1), child.maxWidth(-1)));
+		        }
+
+		        if (prefBaselineComplement != -1) {
+		            double baseline = child.getBaselineOffset();
+		            if (child.isResizable() && baseline == BASELINE_OFFSET_SAME_AS_HEIGHT) {
+		                // When baseline is same as height, the preferred height of the node will be above the baseline, so we need to add
+		                // the preferred complement to it
+		                return top + snapSize(boundedSize(child.minHeight(alt), child.prefHeight(alt), child.maxHeight(alt))) + bottom
+		                        + prefBaselineComplement;
+		            } else {
+		                // For all other Nodes, it's just their baseline and the complement.
+		                // Note that the complement already contain the Node's preferred (or fixed) height
+		                return top + baseline + prefBaselineComplement + bottom;
+		            }
+		        } else {
+		            return top + snapSize(boundedSize(child.minHeight(alt), child.prefHeight(alt), child.maxHeight(alt))) + bottom;
+		        }
+		    }
+		    
+		    
+		    private double snapSpace(double value, boolean snapToPixel) {
+		        return snapToPixel ? Math.round(value) : value;
+		    }
+		    private double boundedSize(double min, double pref, double max) {
+		        double a = pref >= min ? pref : min;
+		        double b = min >= max ? min : max;
+		        return a <= b ? a : b;
+		    }
+
+	}
+	
+	
 }
