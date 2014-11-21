68e9a110ea6af078a3cc8b9620bad94be07f6418
Shadi Shaheen <sshahine@ccc.com.lb>
2014-11-21 10:40:12 +0200

    fixed the scroll if the list is not expanded

--- src/customui/java/components/VirtualFlow.java
+++ src/customui/java/components/VirtualFlow.java
@@ -2328,7 +2328,7 @@ public class VirtualFlow<T extends IndexedCell<?>> extends Region {
             // sure that we DO NOT add empty trailing cells (since we are
             // in the full virtual case and so there are no trailing empty
             // cells).
-            if (! addTrailingCells(false)) {
+            if (! addTrailingCells(true)) {
                 // Reached the end, but not enough cells to fill up to
                 // the end. So, remove the trailing empty space, and translate
                 // the cells down
