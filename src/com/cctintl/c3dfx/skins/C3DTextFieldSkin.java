a43ec5b9fb4dfa1d98c0372fbbfac294cab89a9d
Shadi Shaheen <sshahine@ccc.com.lb>
2015-02-20 17:30:29 +0200

    change text field skin to fix validation messages alignment

--- src/com/cctintl/c3dfx/skins/C3DTextFieldSkin.java
+++ src/com/cctintl/c3dfx/skins/C3DTextFieldSkin.java
@@ -1,5 +1,6 @@
 package com.cctintl.c3dfx.skins;
 
+import javafx.animation.Animation.Status;
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
@@ -8,10 +9,9 @@ import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.value.ChangeListener;
-import javafx.geometry.HPos;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -19,6 +19,7 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
@@ -26,90 +27,168 @@ import javafx.scene.shape.StrokeType;
 import javafx.util.Duration;
 
 import com.cctintl.c3dfx.controls.C3DTextField;
+import com.cctintl.c3dfx.validation.base.ValidatorBase;
 import com.sun.javafx.scene.control.skin.TextFieldSkin;
 
 public class C3DTextFieldSkin extends TextFieldSkin{
 
-	private StackPane effectsPane  = new StackPane();
 	private AnchorPane cursorPane = new AnchorPane();
-	
 	private Line line = new Line();
 	private Line focusedLine = new Line();
 	private Label errorLabel = new Label();
 	private StackPane errorIcon = new StackPane();
-	
+
 	private double endX;
 	private double startX;
 	private double mid ;
 
 	private boolean invalid = true;
+	private HBox errorContainer;
+
+	private double oldErrorLabelHeight = -1;
+	private Pane textPane;
+	private double initYlayout = -1;
+	private double initHeight = -1;
+	private boolean errorShowen = false;
+	private double currentFieldHeight = -1;
+	private double errorLabelInitHeight = 0;
+
+	private Timeline hideErrorAnimation;
+
 
 	public C3DTextFieldSkin(C3DTextField field) {
 		super(field);
-		
+
 		// initial styles
-//		field.setStyle("-fx-background-color: transparent ;-fx-font-weight: BOLD;-fx-prompt-text-fill: #808080;-fx-alignment: top-left ;");
+		//		field.setStyle("-fx-background-color: transparent ;-fx-font-weight: BOLD;-fx-prompt-text-fill: #808080;-fx-alignment: top-left ;");
 		field.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
 		field.setAlignment(Pos.TOP_LEFT);
-		
-		
-		effectsPane.getChildren().add(line);
-		effectsPane.getChildren().add(focusedLine);
-		effectsPane.setAlignment(Pos.BOTTOM_CENTER);
-		StackPane.setMargin(line, new Insets(0,0,1,0));
-		effectsPane.getChildren().add(cursorPane);
-		StackPane.setAlignment(cursorPane, Pos.CENTER_LEFT);
-		StackPane.setMargin(cursorPane, new Insets(0,0,5,40));
-		
+
+
 		errorLabel.getStyleClass().add("errorLabel");
-		errorLabel.setWrapText(true);
+		errorLabel.setWrapText(true);		
 		errorLabel.maxWidthProperty().bind(Bindings.createDoubleBinding(()->field.getWidth()/1.14, field.widthProperty()));
 		errorLabel.minWidthProperty().bind(Bindings.createDoubleBinding(()->field.getWidth()/1.14, field.widthProperty()));
-		
-		StackPane errorLabelContainer = new StackPane();
-		errorLabelContainer.getChildren().add(errorLabel);
-		
-		HBox errorContainer = new HBox();
+//		errorLabel.setStyle("-fx-border-color:BLUE;");
+		AnchorPane errorLabelContainer = new AnchorPane();
+		errorLabelContainer.getChildren().add(errorLabel);		
+
+		errorContainer = new HBox();
 		errorContainer.getChildren().add(errorLabelContainer);
 		errorContainer.getChildren().add(errorIcon);
-		errorIcon.setTranslateY(5);
-		
+		errorIcon.setTranslateY(3);		
 		errorContainer.setSpacing(10);
-		errorContainer.setTranslateY(20);
-		
-		effectsPane.getChildren().add(errorContainer);
-		
-		field.focusedProperty().addListener((o,oldVal,newVal) -> {
-			Platform.runLater(()->{
-				if (newVal) focus();
-				else focusedLine.setVisible(false);	
-			});
+		errorContainer.setTranslateY(25);
+		errorContainer.setVisible(false);		
+		errorContainer.setOpacity(0);
+//		errorContainer.setStyle("-fx-border-color:GREEN;");
+
+		this.getChildren().add(errorContainer);
+
+
+		// add listeners to show error label
+		errorLabel.heightProperty().addListener((o,oldVal,newVal)->{
+			if(errorShowen){
+				if(oldErrorLabelHeight == -1)
+					oldErrorLabelHeight = errorLabelInitHeight = oldVal.doubleValue();
+
+				double newHeight = this.getSkinnable().getHeight() - oldErrorLabelHeight +  newVal.doubleValue();
+				// show the error
+				Timeline errorAnimation = new Timeline(
+						new KeyFrame(Duration.ZERO, new KeyValue(getSkinnable().minHeightProperty(), currentFieldHeight,  Interpolator.EASE_BOTH)),
+						new KeyFrame(Duration.millis(160),
+								// text pane animation
+								new KeyValue(textPane.translateYProperty(), (initYlayout + textPane.getMaxHeight()/2) - newHeight/2, Interpolator.EASE_BOTH),
+								// animate the height change effect
+								new KeyValue(getSkinnable().minHeightProperty(), newHeight, Interpolator.EASE_BOTH)));
+				errorAnimation.play();
+				// show the error label when finished
+				errorAnimation.setOnFinished(finish->new Timeline(new KeyFrame(Duration.millis(160),new KeyValue(errorContainer.opacityProperty(), 1, Interpolator.EASE_BOTH))).play());
+				currentFieldHeight = newHeight;
+				oldErrorLabelHeight = newVal.doubleValue();
+			}	
 		});
-		
+		errorContainer.visibleProperty().addListener((o,oldVal,newVal)->{
+			// show the error label if it's not shown
+			Platform.runLater(()->new Timeline(new KeyFrame(Duration.millis(160),new KeyValue(errorContainer.opacityProperty(), 1, Interpolator.EASE_BOTH))).play());
+		});
+
+
 		field.activeValidatorProperty().addListener((o,oldVal,newVal)->{
 			if(newVal!=null){
-				errorLabel.setText(newVal.getMessage());
-				Node awsomeIcon = newVal.getAwsomeIcon();
-				errorIcon.getChildren().clear();
-				errorIcon.getChildren().add(awsomeIcon);
-				StackPane.setAlignment(awsomeIcon, Pos.TOP_RIGHT);	
-			}else{
-//				field.setPadding(new Insets(0,0,0,0));
-//				HBox.setMargin(errorLabel, new Insets(0,0,0,0));
-				errorLabel.setText(null);
-				errorIcon.getChildren().clear();
-				
+				if(errorContainer.getOpacity() == 0){
+					showError(newVal);
+				}else{
+					hideErrorAnimation = new Timeline(new KeyFrame(Duration.millis(160),new KeyValue(errorContainer.opacityProperty(), 0, Interpolator.EASE_BOTH)));
+					hideErrorAnimation.setOnFinished(finish->{
+						showError(newVal);
+					});
+					hideErrorAnimation.play();
+				}
+			}else{				
+				if(hideErrorAnimation!=null && hideErrorAnimation.getStatus().equals(Status.RUNNING)){
+					Platform.runLater(()->{
+						ChangeListener<Status> listener = new ChangeListener<Status>() {
+							@Override
+							public void changed(ObservableValue<? extends Status> status,Status oldStatus, Status newStatus) {
+								if(newStatus.equals(Status.STOPPED)){
+									hideError();
+									hideErrorAnimation.statusProperty().removeListener(this);
+								}
+							}
+						};			
+						hideErrorAnimation.statusProperty().addListener(listener);					
+					});					
+				}else{
+					hideError();
+				}
 			}
-			invalid = true;
-			Platform.runLater(()->field.requestLayout());
 		});
-		
-		
+
+		field.focusedProperty().addListener((o,oldVal,newVal) -> {
+			if (newVal) focus();
+			else focusedLine.setOpacity(0);	
+		});
+
 		field.prefWidthProperty().addListener((o,oldVal,newVal)-> {
 			field.setMaxWidth(newVal.doubleValue());
 			field.setMinWidth(newVal.doubleValue());
-			invalid = true;	
 		});
+	}
+
+	private void showError(ValidatorBase validator){
+		// set text in error label
+		errorLabel.setText(validator.getMessage());
+		// show error icon
+		Node awsomeIcon = validator.getAwsomeIcon();
+		errorIcon.getChildren().clear();
+		if(awsomeIcon!=null){
+			errorIcon.getChildren().add(awsomeIcon);
+			StackPane.setAlignment(awsomeIcon, Pos.TOP_RIGHT);	
+		}
+		// init only once, to fix the text pane from resizing
+		if(initYlayout == -1){
+			textPane.setMaxHeight(textPane.getHeight());
+			initYlayout = textPane.getBoundsInParent().getMinY(); 
+			initHeight = getSkinnable().getHeight();
+			currentFieldHeight = initHeight;
+		}
+		errorContainer.setVisible(true);
+		errorShowen = true;
+	}
+	
+	private void hideError(){		
+		new Timeline(new KeyFrame(Duration.millis(160), new KeyValue(textPane.translateYProperty(), 0, Interpolator.EASE_BOTH))).play();
+		// rest the height of text field
+		new Timeline(new KeyFrame(Duration.millis(160), new KeyValue(getSkinnable().minHeightProperty(), initHeight, Interpolator.EASE_BOTH))).play();
+		// clear error label text
+		errorLabel.setText(null);
+		oldErrorLabelHeight = errorLabelInitHeight;
+		// clear error icon
+		errorIcon.getChildren().clear();
+		// hide error container
+		errorContainer.setVisible(false);
+		errorShowen = false;	
 	}
 
 	@Override protected double computePrefHeight(double width, double topInset, double rightInset, double bottomInset, double leftInset) {
@@ -127,16 +206,18 @@ public class C3DTextFieldSkin extends TextFieldSkin{
 	@Override 
 	protected void layoutChildren(final double x, final double y, final double w, final double h) {
 		super.layoutChildren(x, y, w, h);
-		if(invalid){
-			startX = getSkinnable().getBoundsInLocal().getMinX() ;
-			endX = getSkinnable().getWidth() - getSkinnable().getBaselineOffset();
 
-			line.setStartX( startX );
-			line.setEndX(endX);
-			line.setStartY(getSkinnable().getBoundsInLocal().getMaxY() );
-			line.setEndY(getSkinnable().getBoundsInLocal().getMaxY() );
-			line.strokeProperty().bind(((C3DTextField)getSkinnable()).unFocusColorProperty());			
+		if(invalid){
+
+			textPane = ((Pane)this.getChildren().get(0));
+
+			line.setStartX(0);
+			line.endXProperty().bind(textPane.widthProperty());
+			line.startYProperty().bind(textPane.heightProperty());
+			line.endYProperty().bind(line.startYProperty());
+			line.strokeProperty().bind(((C3DTextField)getSkinnable()).unFocusColorProperty());
 			line.setStrokeWidth(1);
+			line.setTranslateY(-2);
 			line.setStrokeType(StrokeType.CENTERED);
 			if(getSkinnable().isDisabled()) line.getStrokeDashArray().addAll(2d);
 			getSkinnable().disabledProperty().addListener((o,oldVal,newVal) -> {
@@ -144,31 +225,52 @@ public class C3DTextFieldSkin extends TextFieldSkin{
 				if(newVal)
 					line.getStrokeDashArray().addAll(2d);
 			});
-			
-			
-			mid = (endX - startX )/2;			
+
+			textPane.widthProperty().addListener((o,oldVal,newVal)->{
+				startX = 0;
+				endX = newVal.doubleValue();
+				mid = (endX - startX )/2;
+				focusedLine.setStartX(mid);
+				focusedLine.setEndX(mid);
+				System.out.println(startX);
+			});
+
+			startX = 0;
+			endX = textPane.getWidth();
+			mid = (endX - startX )/2;
 			focusedLine.setStartX(mid);
 			focusedLine.setEndX(mid);
-			focusedLine.setStartY(getSkinnable().getBoundsInLocal().getMaxY() );
-			focusedLine.setEndY(getSkinnable().getBoundsInLocal().getMaxY() );
+			focusedLine.startYProperty().bind(line.startYProperty());
+			focusedLine.endYProperty().bind(line.startYProperty());
 			focusedLine.strokeProperty().bind(((C3DTextField)getSkinnable()).focusColorProperty());
 			focusedLine.setStrokeWidth(2);
+			focusedLine.setTranslateY(-1);
 			focusedLine.setStrokeType(StrokeType.CENTERED);
-			focusedLine.setVisible(false);
+			focusedLine.setOpacity(0);
 
-			//			cursorPane.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
-			cursorPane.setMaxSize(40, getSkinnable().getHeight());
-			cursorPane.setBackground(new Background(new BackgroundFill(((C3DTextField)getSkinnable()).getFocusColor(), CornerRadii.EMPTY, Insets.EMPTY)));
-			cursorPane.setOpacity(0);
-			
-			this.getChildren().remove(effectsPane);
-			this.getChildren().add(effectsPane);
-		
-			layoutInArea(effectsPane, x, y, w , h, -1, HPos.CENTER, VPos.BOTTOM);
-			
+			line.translateXProperty().bind(Bindings.createDoubleBinding(()-> -focusedLine.getStrokeWidth(), focusedLine.strokeWidthProperty()));
+			focusedLine.translateXProperty().bind(Bindings.createDoubleBinding(()-> -focusedLine.getStrokeWidth(), focusedLine.strokeWidthProperty()));
+
+
+			textPane.getChildren().remove(line);
+			textPane.getChildren().add(line);
+
+			textPane.getChildren().remove(focusedLine);
+			textPane.getChildren().add(focusedLine);
+
+			cursorPane.setMaxSize(40, textPane.getHeight() - 5);
+			cursorPane.setMinSize(40, textPane.getHeight() - 5);
+			cursorPane.backgroundProperty().bind(Bindings.createObjectBinding(()-> new Background(new BackgroundFill(((C3DTextField)getSkinnable()).getFocusColor(), CornerRadii.EMPTY, Insets.EMPTY)), ((C3DTextField)getSkinnable()).focusColorProperty()));
+			cursorPane.setTranslateX(40);
+			cursorPane.setVisible(false);
+
+			textPane.getChildren().remove(cursorPane);
+			textPane.getChildren().add(cursorPane);
+
+
 			invalid = false;
 		}		
-		
+
 	}
 
 	private void focus(){
@@ -176,12 +278,12 @@ public class C3DTextFieldSkin extends TextFieldSkin{
 				new KeyFrame(
 						Duration.ZERO,       
 						new KeyValue(focusedLine.startXProperty(), mid ,Interpolator.EASE_BOTH),
-						new KeyValue(focusedLine.visibleProperty(), false ,Interpolator.EASE_BOTH),									
+						new KeyValue(focusedLine.opacityProperty(), 0 ,Interpolator.EASE_BOTH),									
 						new KeyValue(focusedLine.endXProperty(), mid ,Interpolator.EASE_BOTH)
 						),
 						new KeyFrame(
 								Duration.millis(5),
-								new KeyValue(focusedLine.visibleProperty(), true ,Interpolator.EASE_BOTH)
+								new KeyValue(focusedLine.opacityProperty(), 1 ,Interpolator.EASE_BOTH)
 								),
 								new KeyFrame(
 										Duration.millis(150),
@@ -196,7 +298,7 @@ public class C3DTextFieldSkin extends TextFieldSkin{
 						Duration.ZERO,       
 						new KeyValue(cursorPane.visibleProperty(), false ,Interpolator.EASE_BOTH),
 						new KeyValue(cursorPane.scaleXProperty(), 1 ,Interpolator.EASE_BOTH),
-						new KeyValue(cursorPane.translateXProperty(), 0 ,Interpolator.EASE_BOTH),
+						new KeyValue(cursorPane.translateXProperty(), 40 ,Interpolator.EASE_BOTH),
 						new KeyValue(cursorPane.opacityProperty(), 0.75 ,Interpolator.EASE_BOTH)
 						),
 						new KeyFrame(
@@ -206,7 +308,7 @@ public class C3DTextFieldSkin extends TextFieldSkin{
 								new KeyFrame(
 										Duration.millis(150),
 										new KeyValue(cursorPane.scaleXProperty(), 1/cursorPane.getWidth() ,Interpolator.EASE_BOTH),
-										new KeyValue(cursorPane.translateXProperty(), -65 ,Interpolator.EASE_BOTH),
+										new KeyValue(cursorPane.translateXProperty(), -40 ,Interpolator.EASE_BOTH),
 										new KeyValue(cursorPane.opacityProperty(), 0 ,Interpolator.EASE_BOTH)
 										)
 
