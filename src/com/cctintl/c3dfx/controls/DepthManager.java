d10c56bc3078bcc12b92b71ab9820bdfa74306a7
Shadi Shaheen <sshahine@ccc.com.lb>
2015-05-13 11:33:13 +0300

    adding CCT Copyrights

--- src/com/cctintl/c3dfx/controls/DepthManager.java
+++ src/com/cctintl/c3dfx/controls/DepthManager.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2015, CCT and/or its affiliates. All rights reserved.
+ * CCT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
 package com.cctintl.c3dfx.controls;
 
 import javafx.scene.Node;
