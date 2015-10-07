88e1f13f84d3341c6df4a7d66e38cedeac216d8b
Shadi Shaheen <sshahine@ccc.com.lb>
2015-10-07 20:44:44 +0300

    Apply license headers to all files

--- src/com/jfoenix/skins/JFXComboBoxListViewSkin.java
+++ src/com/jfoenix/skins/JFXComboBoxListViewSkin.java
@@ -40,6 +40,24 @@ import javafx.scene.control.ComboBoxBase;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+/*
+ * JFoenix
+ * Copyright (c) 2015, JFoenix and/or its affiliates., All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
 import javafx.scene.control.SelectionModel;
 import javafx.scene.control.TextField;
 import javafx.scene.input.DragEvent;
--- src/com/jfoenix/svg/SVGGlyph.java
+++ src/com/jfoenix/svg/SVGGlyph.java
@@ -1,26 +1,19 @@
 /*
- * Copyright (c) 2015, JFoenix and/or its affiliates. All rights reserved.
- * JFoenix PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * JFoenix
+ * Copyright (c) 2015, JFoenix and/or its affiliates., All rights reserved.
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
  *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
  */
 
 package com.jfoenix.svg;
