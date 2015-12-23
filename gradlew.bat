16a62a8bd99bda69a0b975ec4540e47af35bba0b
yangyining <yangyining@janeluo.net>
2015-12-24 00:38:24 +0800

    设置JAVAHOME

--- gradlew.bat
+++ gradlew.bat
@@ -7,7 +7,7 @@
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
-
+SET JAVA_HOME=E:\Java\jre1.8.0_66
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
