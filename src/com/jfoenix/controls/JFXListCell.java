bdefbdc4563f9688fed0c4b5b7110046d10d1808
Shadi Shaheen <sshahine@ccc.com.lb>
2016-03-21 12:37:05 +0200

    fixed minor issue in JFXListCell related to the height of the cell

--- src/com/jfoenix/controls/JFXListCell.java
+++ src/com/jfoenix/controls/JFXListCell.java
@@ -259,7 +259,7 @@ public class JFXListCell<T> extends ListCell<T> {
 //								borderWidth += listview.getPadding().getTop();
 //								borderWidth += listview.getPadding().getBottom();
 //							}
-							double newHeight = (this.getHeight() + listview.currentVerticalGapProperty().get()) * listview.getItems().size() + this.snappedTopInset() + this.snappedBottomInset() - listview.currentVerticalGapProperty().get();
+							double newHeight = (this.getHeight() + listview.currentVerticalGapProperty().get()) * listview.getItems().size() + listview.snappedTopInset() + listview.snappedBottomInset() - listview.currentVerticalGapProperty().get();
 							/*
 							 *  expanding list will ignore its maxheight property. 
 							 *  the maxHeight property shouldn't be set by the user, otherwise
