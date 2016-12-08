9245e499204788a693529eca80c8e59bbb97f4e3
Shadi Shaheen <sshahine@ccc.com.lb>
2016-12-08 16:18:21 +0200

    fixed concurrency issue when caching nodes for animation

--- src/com/jfoenix/transitions/CacheMomento.java
+++ src/com/jfoenix/transitions/CacheMomento.java
@@ -18,11 +18,15 @@
  */
 package com.jfoenix.transitions;
 
+import java.util.HashMap;
+
 import javafx.scene.CacheHint;
 import javafx.scene.Node;
 import javafx.scene.layout.Region;
 
 public class CacheMomento{
+	private static HashMap<Node, CacheMomento> history = new HashMap<>();
+	
 	private boolean cache;
 	private boolean cacheShape;
 	private boolean snapToPixel;
@@ -32,18 +36,30 @@ public class CacheMomento{
 	public CacheMomento(Node node) {
 		this.node = node;
 	}
-	
+	/**
+	 * this method will cache the node only if it wasn't cached before
+	 */
 	public void cache(){
-		this.cache = node.isCache();
-		this.cacheHint = node.getCacheHint();
-		node.setCache(true);
-		node.setCacheHint(CacheHint.SPEED);
-		if(node instanceof Region){
-			this.cacheShape = ((Region)node).isCacheShape();
-			this.snapToPixel = ((Region)node).isSnapToPixel();
-			((Region)node).setCacheShape(true);
-			((Region)node).setSnapToPixel(true);
+		if(!history.containsKey(node)){
+			this.cache = node.isCache();
+			this.cacheHint = node.getCacheHint();
+			node.setCache(true);
+			node.setCacheHint(CacheHint.SPEED);
+			if(node instanceof Region){
+				this.cacheShape = ((Region)node).isCacheShape();
+				this.snapToPixel = ((Region)node).isSnapToPixel();
+				((Region)node).setCacheShape(true);
+				((Region)node).setSnapToPixel(true);
+			}
+			history.put(node, this);
+		}else{
+			CacheMomento cached = new CacheMomento(node);
+			this.cache = cached.cache;
+			this.cacheHint = cached.cacheHint;
+			this.cacheShape = cached.cacheShape;
+			this.snapToPixel = cached.snapToPixel;
 		}
+		
 	}
 	
 	public void restore(){
