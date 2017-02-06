9beda40c37d4e89db7bc65e7e87ee7a3ab480a0e
Shadi Shaheen <sshahine@ccc.com.lb>
2017-02-06 16:41:36 +0200

    update header scroll pane computations

--- src/com/jfoenix/controls/JFXScrollPane.java
+++ src/com/jfoenix/controls/JFXScrollPane.java
@@ -19,7 +19,6 @@
 package com.jfoenix.controls;
 
 import javafx.beans.DefaultProperty;
-import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -33,6 +32,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Scale;
+import javafx.scene.transform.Transform;
 
 /**
  * <h1>Material Design ScrollPane with header </h1>
@@ -46,34 +46,35 @@ import javafx.scene.transform.Scale;
 public class JFXScrollPane extends StackPane{
 
 	private static final String DEFAULT_STYLE_CLASS = "jfx-scroll-pane";
-	
+
 	private ScrollPane scrollPane = new ScrollPane();
 	private VBox contentContainer = new VBox();
 	private StackPane headerSpace = new StackPane();
 	private StackPane condensedHeaderBG = new StackPane();
 	private StackPane headerBG = new StackPane();
-	
+
 	private double initY = -1;
 	private double maxHeight = -1;
 	private double minHeight = -1;
 
 	private StackPane bottomBar;
 	Scale scale = new Scale(1, 1, 0, 0);
-
+	Transform oldSceneTransform = null;
+	
 	//	private Timeline animation;
 	private StackPane midBar;
 	private StackPane topBar;
 
 	public JFXScrollPane() {
 		this.getStyleClass().add(DEFAULT_STYLE_CLASS);
-		
+
 		// clip content
 		Rectangle clip = new Rectangle();
 		this.setClip(clip);
 		clip.widthProperty().bind(this.widthProperty());
 		clip.heightProperty().bind(this.heightProperty());
 
-		
+
 		StackPane header = new  StackPane();
 		condensedHeaderBG.setOpacity(0);
 		condensedHeaderBG.getStyleClass().add("condensed-header");
@@ -111,56 +112,56 @@ public class JFXScrollPane extends StackPane{
 		headerSpace.setFocusTraversable(true);
 
 		contentContainer.getChildren().setAll(headerSpace);
-
+		
+		contentContainer.localToSceneTransformProperty().addListener((o,oldVal,newVal)-> oldSceneTransform = oldVal);
 		scrollPane.setContent(contentContainer);
 		scrollPane.setFitToWidth(true);
 		scrollPane.vvalueProperty().addListener((o,oldVal,newVal)->{
-
-			double ty = newVal.doubleValue() * scrollPane.getContent().getLayoutBounds().getHeight();
-			double opacity = ty / minHeight;
-			opacity = opacity > 1 ? 1 : (opacity < 0)? 0 : opacity;
-			Bounds localToScene = headerSpace.localToScene(headerSpace.getBoundsInLocal());
-
 			if(minHeight == -1) {
 				minHeight = bottomBar.getBoundsInParent().getMinY();
 				maxHeight = header.getHeight();
-				initY = localToScene.getMinY();
 			}
-			
+			if(initY == -1){
+				initY = oldSceneTransform.getTy();
+			}
+
+			// translation
+			double ty = contentContainer.getLocalToSceneTransform().getTy();
+			double opacity = Math.abs(ty - initY) / minHeight;
+			opacity = opacity > 1 ? 1 : (opacity < 0)? 0 : opacity;
 			// update properties according to the scroll value
 			// opacity
 			headerBG.setOpacity(1 - opacity);
 			condensedHeaderBG.setOpacity(opacity);
-
-			// translation
-			double currentY = localToScene.getMinY() - initY;
-			topBar.setTranslateY(-currentY <= minHeight ? -currentY : minHeight);
-			if(newVal.doubleValue()!= 1 && newVal.doubleValue() < oldVal.doubleValue() && -currentY > minHeight){
-				double oldTy = oldVal.doubleValue() * scrollPane.getContent().getLayoutBounds().getHeight();
-				double diff =  oldTy - ty;
-				if(-(header.getTranslateY() + diff) > minHeight)
-					header.setTranslateY(header.getTranslateY() + diff);
-				else header.setTranslateY(-minHeight);
+			
+			if(newVal.doubleValue() == 0){
+				header.setTranslateY(0);
+				topBar.setTranslateY(0);
+			}else if(newVal.doubleValue() == 1){
+				topBar.setTranslateY(minHeight);
+				header.setTranslateY(-maxHeight);
 			}else{
-				if(-currentY > maxHeight){
-					double oldTy = oldVal.doubleValue() * scrollPane.getContent().getLayoutBounds().getHeight();
-					double diff =  oldTy - ty;
-					if(-(header.getTranslateY() + diff) < maxHeight)
-						header.setTranslateY(header.getTranslateY() + diff);
-					else header.setTranslateY(-maxHeight);
+				double dy = ty - initY;
+				topBar.setTranslateY(-dy <= minHeight ? -dy : minHeight);
+				
+				double oldTy = oldSceneTransform.getTy();
+				double diff =  oldTy - ty;
+				
+				if(newVal.doubleValue() < oldVal.doubleValue() && -dy > minHeight){
+					if(-(header.getTranslateY() - diff) > minHeight)
+						header.setTranslateY(header.getTranslateY() - diff);
+					else header.setTranslateY(-minHeight);
 				}else{
-					if(newVal.doubleValue() != 0){
-						double oldTy = oldVal.doubleValue() * scrollPane.getContent().getLayoutBounds().getHeight();
-						double diff =  oldTy - ty;
-						if(diff > maxHeight) 
-							header.setTranslateY(-maxHeight);
-						else
-							header.setTranslateY(-currentY < maxHeight ? currentY : -maxHeight);
-					}else
-						header.setTranslateY(0);
+					if(-dy > maxHeight){
+						if(-(header.getTranslateY() - diff) < maxHeight)
+							header.setTranslateY(header.getTranslateY() - diff);
+						else header.setTranslateY(-maxHeight);
+					}else{
+						if(diff > maxHeight) header.setTranslateY(-maxHeight);
+						else header.setTranslateY(dy);
+					}
 				}
 			}
-			
 			// scale 
 			scale.setX(map(opacity, 0, 1, 1, 0.75));
 			scale.setY(map(opacity, 0, 1, 1, 0.75));
@@ -185,23 +186,15 @@ public class JFXScrollPane extends StackPane{
 	public Node getContent(){
 		return contentContainer.getChildren().size() == 2? contentContainer.getChildren().get(1) : null;
 	}
-	
-//	public void setTopBarContent(Node node){
-//		topBar.getChildren().setAll(node);
-//	}
+
 	public StackPane getTopBar(){
 		return topBar;
 	}
-//	public void setMidBar(Node node){
-//		midBar.getChildren().setAll(node);
-//	}
+ 
 	public StackPane getMidBar(){
 		return midBar;
-//		return midBar.getChildren().isEmpty()? null : midBar.getChildren().get(0);
 	}
-//	public void setBottomBar(Node node){
-//		bottomBar.getChildren().setAll(node);
-//	}
+	
 	public StackPane getBottomBar(){
 		return bottomBar;
 	}
