a95bac34e3e220182f5d9f60d9157eccf3f23ac5
thetric <broj.dominik@gmail.com>
2017-03-23 15:22:36 +0100

    move 'demo' files to /demo/src/main

---
+++ demo/src/main/java/demos/gui/uicomponents/ButtonController.java
@@ -0,0 +1,17 @@
+package demos.gui.uicomponents;
+
+import io.datafx.controller.ViewController;
+
+@ViewController(value = "/fxml/ui/Button.fxml", title = "Material Design Example")
+public class ButtonController {
+
+//	@FXMLViewFlowContext
+//	private ViewFlowContext context;
+//
+//	@PostConstruct
+//	public void init() throws FlowException, VetoException {
+//		if(((Pane) context.getRegisteredObject("ContentPane")).getChildren().size() > 0)
+//			Platform.runLater(()-> ((Pane)((Pane) context.getRegisteredObject("ContentPane")).getChildren().get(0)).getChildren().remove(1));
+//	}
+
+}
---
+++ demo/src/main/java/demos/gui/uicomponents/SliderController.java
@@ -0,0 +1,8 @@
+package demos.gui.uicomponents;
+
+import io.datafx.controller.ViewController;
+
+@ViewController(value = "/fxml/ui/Slider.fxml", title = "Material Design Example")
+public class SliderController {
+
+}
