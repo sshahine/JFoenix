7453020135013a49cd126faaa9dc358753993a56
Shadi Shaheen <sshahine@ccc.com.lb>
2017-08-26 21:26:43 +0300

    compute initial scene size according to the screen size

--- demo/src/main/java/demos/MainDemo.java
+++ demo/src/main/java/demos/MainDemo.java
@@ -9,7 +9,9 @@ import io.datafx.controller.flow.context.FXMLViewFlowContext;
 import io.datafx.controller.flow.context.ViewFlowContext;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -42,7 +44,16 @@ public class MainDemo extends Application {
 
         JFXDecorator decorator = new JFXDecorator(stage, container.getView());
         decorator.setCustomMaximize(true);
-        Scene scene = new Scene(decorator, 800, 600);
+
+        double width = 800;
+        double height = 600;
+        try {
+            Rectangle2D bounds = Screen.getScreens().get(0).getBounds();
+            width = bounds.getWidth() / 2.5;
+            height = bounds.getHeight() / 1.35;
+        }catch (Exception e){ }
+
+        Scene scene = new Scene(decorator, width, height);
         final ObservableList<String> stylesheets = scene.getStylesheets();
         stylesheets.addAll(MainDemo.class.getResource("/css/jfoenix-fonts.css").toExternalForm(),
                            MainDemo.class.getResource("/css/jfoenix-design.css").toExternalForm(),
