4113c7681ce69254ec0be466eb508fcd94786325
Shadi Shaheen <sshahine@ccc.com.lb>
2017-11-02 15:31:31 +0200

    added header

--- jfoenix/src/main/java/com/jfoenix/utils/JFXNodeUtils.java
+++ jfoenix/src/main/java/com/jfoenix/utils/JFXNodeUtils.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.jfoenix.utils;
 
 import javafx.animation.PauseTransition;
@@ -13,6 +32,11 @@ import javafx.util.Duration;
 
 import java.util.Locale;
 
+/**
+ * @author Shadi Shaheen
+ * @version 1.0
+ * @since 2017-02-11
+ */
 public class JFXNodeUtils {
 
 
