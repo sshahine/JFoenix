1a0b008b159cba2dfc78a0fed5456f0bab9549b5
Shadi Shaheen <sshahine@ccc.com.lb>
2018-02-21 11:33:18 +0200

    added mini drawer size to JFXDrawer / updated drawer status methods names to match the events

--- jfoenix/src/main/java/com/jfoenix/controls/JFXDrawersStack.java
+++ jfoenix/src/main/java/com/jfoenix/controls/JFXDrawersStack.java
@@ -20,12 +20,13 @@
 package com.jfoenix.controls;
 
 import com.jfoenix.utils.JFXNodeUtils;
-import javafx.animation.PauseTransition;
-import javafx.application.Platform;
 import javafx.beans.DefaultProperty;
-import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
@@ -36,15 +37,18 @@ import java.util.List;
 /**
  * DrawersStack is used to show multiple drawers at once
  *
+ * UPDATE : DrawersStack extends Region instead of StackPane to
+ * encapsulate the getChildren() method and hide it from the user.
  * @author Shadi Shaheen
  * @version 1.0
  * @since 2016-03-09
  */
 @DefaultProperty(value = "content")
-public class JFXDrawersStack extends StackPane {
+public class JFXDrawersStack extends Region {
 
     private List<JFXDrawer> drawers = new ArrayList<>();
     private Node content;
+    private boolean performingLayout;
 
     /**
      * creates empty drawers stack
@@ -54,6 +58,34 @@ public class JFXDrawersStack extends StackPane {
         clip.widthProperty().bind(this.widthProperty());
         clip.heightProperty().bind(this.heightProperty());
         this.setClip(clip);
+    }
+
+
+    @Override public void requestLayout() {
+        if (performingLayout) {
+            return;
+        }
+        super.requestLayout();
+    }
+
+    @Override protected void layoutChildren() {
+        performingLayout = true;
+        List<Node> managed = getManagedChildren();
+        final double width = getWidth();
+        double height = getHeight();
+        double top = getInsets().getTop();
+        double right = getInsets().getRight();
+        double left = getInsets().getLeft();
+        double bottom = getInsets().getBottom();
+        double contentWidth = width - left - right;
+        double contentHeight = height - top - bottom;
+        for (int i = 0, size = managed.size(); i < size; i++) {
+            Node child = managed.get(i);
+            layoutInArea(child, left, top, contentWidth, contentHeight,
+                0, Insets.EMPTY,
+                HPos.CENTER, VPos.CENTER);
+        }
+        performingLayout = false;
     }
 
     /**
@@ -81,9 +113,11 @@ public class JFXDrawersStack extends StackPane {
     /**
      * add JFXDrawer to the stack
      *
+     * NOTE: this method will also be called when {@link JFXDrawersStack#toggle(JFXDrawer)} to add
+     * the drawer if not added
      * @param drawer
      */
-    private void addDrawer(JFXDrawer drawer) {
+    public void addDrawer(JFXDrawer drawer) {
         if (drawer == null) {
             return;
         }
@@ -137,7 +171,7 @@ public class JFXDrawersStack extends StackPane {
         if (!drawers.contains(drawer)) {
             addDrawer(drawer);
         }
-        if (drawer.isShown() || drawer.isShowing()) {
+        if (drawer.isOpened() || drawer.isOpening()) {
             drawer.close();
         } else {
             updateDrawerPosition(drawer);
@@ -156,11 +190,11 @@ public class JFXDrawersStack extends StackPane {
             addDrawer(drawer);
         }
         if (!show) {
-            if (drawer.isShown() || drawer.isShowing()) {
+            if (drawer.isOpened() || drawer.isOpening()) {
                 drawer.close();
             }
         } else {
-            if (!drawer.isShown() && !drawer.isShowing()) {
+            if (!drawer.isOpened() && !drawer.isOpening()) {
                 updateDrawerPosition(drawer);
                 drawer.open();
             }
