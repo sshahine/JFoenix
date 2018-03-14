56d898244be0b7ce8988d1179dd0ffa1e99206b7
Shadi Shaheen <sshahine@ccc.com.lb>
2018-03-14 15:11:28 +0200

    added press and hold filter to JFXNodeUtils

--- jfoenix/src/main/java/com/jfoenix/utils/JFXNodeUtils.java
+++ jfoenix/src/main/java/com/jfoenix/utils/JFXNodeUtils.java
@@ -83,4 +83,22 @@ public class JFXNodeUtils {
         node.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> holdTimer.stop());
         node.addEventHandler(MouseEvent.DRAG_DETECTED, event -> holdTimer.stop());
     }
+
+    public static void addPressAndHoldFilter(Node node, Duration holdTime,
+                                              EventHandler<MouseEvent> handler) {
+        class Wrapper<T> {
+            T content;
+        }
+        Wrapper<MouseEvent> eventWrapper = new Wrapper<>();
+
+        PauseTransition holdTimer = new PauseTransition(holdTime);
+        holdTimer.setOnFinished(event -> handler.handle(eventWrapper.content));
+
+        node.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> {
+            eventWrapper.content = event;
+            holdTimer.playFromStart();
+        });
+        node.addEventFilter(MouseEvent.MOUSE_RELEASED, event -> holdTimer.stop());
+        node.addEventFilter(MouseEvent.DRAG_DETECTED, event -> holdTimer.stop());
+    }
 }
