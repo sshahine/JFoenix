02736f558fb57c85f5e707c441485d2b0f225326
Shadi Shaheen <sshahine@ccc.com.lb>
2018-03-14 13:17:18 +0200

    removed arrow animation from JFXTreeTableRowSkin (it's buggy + extra unneeded overhead)

--- jfoenix/src/main/java/com/jfoenix/skins/JFXTreeTableRowSkin.java
+++ jfoenix/src/main/java/com/jfoenix/skins/JFXTreeTableRowSkin.java
@@ -21,15 +21,11 @@ package com.jfoenix.skins;
 
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import com.sun.javafx.scene.control.skin.TreeTableRowSkin;
-import javafx.animation.*;
-import javafx.animation.Animation.Status;
-import javafx.beans.value.ChangeListener;
 import javafx.css.PseudoClass;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Control;
 import javafx.scene.control.TreeTableRow;
-import javafx.util.Duration;
 
 import java.lang.reflect.Field;
 import java.util.Map;
@@ -39,22 +35,9 @@ import java.util.Map;
  */
 public class JFXTreeTableRowSkin<T> extends TreeTableRowSkin<T> {
 
+    private static final PseudoClass groupedClass = PseudoClass.getPseudoClass("grouped");
+
     static Map<Control, Double> disclosureWidthMap = null;
-
-    // this vairable is used to hold the expanded/collapsed row index
-    private static int expandedIndex = -1;
-    // this variable indicates whether an expand/collapse operation is triggered
-    private boolean expandTriggered = false;
-
-
-    private ChangeListener<Boolean> expandedListener = (o, oldVal, newVal) -> {
-        if (getSkinnable().getTreeItem() != null && !getSkinnable().getTreeItem().isLeaf()) {
-            expandedIndex = getSkinnable().getIndex();
-            expandTriggered = true;
-        }
-    };
-    private Timeline collapsedAnimation;
-    private Animation expandedAnimation;
 
     public JFXTreeTableRowSkin(TreeTableRow<T> control) {
         super(control);
@@ -70,38 +53,26 @@ public class JFXTreeTableRowSkin<T> extends TreeTableRowSkin<T> {
                 ex.printStackTrace();
             }
         }
-        getSkinnable().indexProperty().addListener((o, oldVal, newVal) -> {
-            if (newVal.intValue() != -1) {
-                if (newVal.intValue() == expandedIndex) {
-                    expandTriggered = true;
-                    expandedIndex = -1;
-                } else {
-                    expandTriggered = false;
-                }
-            }
-        });
 
         // clear disclosure node indentation after grouping/ungrouping
         getSkinnable().getTreeTableView().rootProperty().addListener((o, oldVal, newVal) -> {
             disclosureWidthMap.remove(getSkinnable().getTreeTableView());
         });
-    }
 
-    private static final PseudoClass groupedClass = PseudoClass.getPseudoClass("grouped");
+        // allow custom skin to grouped rows
+        getSkinnable().itemProperty().addListener(observable -> {
+            T item = getSkinnable().getItem();
+            pseudoClassStateChanged(groupedClass, item != null
+                                                  && item instanceof RecursiveTreeObject
+                                                  && item.getClass() == RecursiveTreeObject.class);
+        });
+    }
 
     @Override
     protected void layoutChildren(final double x, final double y, final double w, final double h) {
-        // allow custom skin to grouped rows
-        pseudoClassStateChanged(groupedClass, false);
-        if (getSkinnable().getTreeItem() != null
-            && getSkinnable().getTreeItem().getValue() instanceof RecursiveTreeObject
-            && getSkinnable().getTreeItem().getValue().getClass() == RecursiveTreeObject.class) {
-            pseudoClassStateChanged(groupedClass, true);
-        }
-
+        super.layoutChildren(x, y, w, h);
         if (getSkinnable().getIndex() > -1 && getSkinnable().getTreeTableView()
-            .getTreeItem(getSkinnable().getIndex()) != null) {
-            super.layoutChildren(x, y, w, h);
+                                                  .getTreeItem(getSkinnable().getIndex()) != null) {
 
             // disclosure row case
             if (getSkinnable().getTreeItem() != null && !getSkinnable().getTreeItem().isLeaf()) {
@@ -110,7 +81,7 @@ public class JFXTreeTableRowSkin<T> extends TreeTableRowSkin<T> {
                 final Parent arrowParent = arrow.getParent();
                 if (((RecursiveTreeObject<?>) getSkinnable().getItem()).getGroupedColumn() != null) {
                     Node col = getChildren().get(getSkinnable().getTreeTableView()
-                        .getTreeItemLevel(getSkinnable().getTreeItem()) + 1);
+                                                     .getTreeItemLevel(getSkinnable().getTreeItem()) + 1);
                     if (getSkinnable().getItem() instanceof RecursiveTreeObject) {
                         int index = getSkinnable().getTreeTableView()
                             .getColumns()
@@ -124,48 +95,8 @@ public class JFXTreeTableRowSkin<T> extends TreeTableRowSkin<T> {
                     arrowParent.setTranslateX(0);
                     arrowParent.setLayoutX(0);
                 }
-
-                // add disclosure node animation
-                if (expandedAnimation == null || !(expandedAnimation.getStatus() == Status.RUNNING)) {
-                    expandedAnimation = new Timeline(new KeyFrame(Duration.millis(160),
-                        new KeyValue(arrow.rotateProperty(),
-                            90,
-                            Interpolator.EASE_BOTH)));
-                    expandedAnimation.setOnFinished((finish) -> arrow.setRotate(90));
-                }
-                if (collapsedAnimation == null || !(collapsedAnimation.getStatus() == Status.RUNNING)) {
-                    collapsedAnimation = new Timeline(new KeyFrame(Duration.millis(160),
-                        new KeyValue(arrow.rotateProperty(),
-                            0,
-                            Interpolator.EASE_BOTH)));
-                    collapsedAnimation.setOnFinished((finish) -> arrow.setRotate(0));
-                }
-                getSkinnable().getTreeItem().expandedProperty().removeListener(expandedListener);
-                getSkinnable().getTreeItem().expandedProperty().addListener(expandedListener);
-
-                if (expandTriggered) {
-                    if (getSkinnable().getTreeTableView().getTreeItem(getSkinnable().getIndex()).isExpanded()) {
-                        arrow.setRotate(0);
-                        expandedAnimation.play();
-                    } else {
-                        arrow.setRotate(90);
-                        collapsedAnimation.play();
-                    }
-                    expandTriggered = false;
-                } else {
-                    if (getSkinnable().getTreeTableView().getTreeItem(getSkinnable().getIndex()).isExpanded()) {
-                        if (expandedAnimation.getStatus() != Status.RUNNING) {
-                            arrow.setRotate(90);
-                        }
-                    } else {
-                        if (collapsedAnimation.getStatus() != Status.RUNNING) {
-                            arrow.setRotate(0);
-                        }
-                    }
-                }
             }
         }
-
     }
 
     @Override
