e9a232e68acca6a62cb9608f07b0725fbb4d7ec5
Shadi Shaheen <sshahine@ccc.com.lb>
2018-09-05 09:51:23 +0300

    added method to JFXDrawersStack to get the current sides order

--- jfoenix/src/main/java/com/jfoenix/controls/JFXDrawersStack.java
+++ jfoenix/src/main/java/com/jfoenix/controls/JFXDrawersStack.java
@@ -23,11 +23,10 @@ import com.jfoenix.utils.JFXNodeUtils;
 import javafx.beans.DefaultProperty;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.geometry.Side;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
@@ -201,5 +200,17 @@ public class JFXDrawersStack extends Region {
         }
     }
 
-
+    /**
+     * @return a list of sides that corresponds to the current drawers order
+     */
+    public List<Side> getOpenedDrawersOrder(){
+        List<Side> order = new ArrayList<>();
+        for (int i = 0, drawersSize = drawers.size(); i < drawersSize; i++) {
+            final JFXDrawer jfxDrawer = drawers.get(i);
+            if (jfxDrawer.isOpened()) {
+                order.add(Side.valueOf(jfxDrawer.getDirection().toString()));
+            }
+        }
+        return order;
+    }
 }
